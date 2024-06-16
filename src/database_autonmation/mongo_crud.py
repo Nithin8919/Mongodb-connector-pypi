2ac39b5ca7eb21b63c94522710d487d7d271f711
Nithin <cherukumallinithin@gmail.com>
2024-06-16 18:24:34 +0530

    day 2.2

--- src/database_autonmation/mongo_crud.py
+++ src/database_autonmation/mongo_crud.py
@@ -1,12 +1,4 @@
-from typing import Any
-import os
-import pandas as pd
-import pymongo
-import json
-from ensure import ensure_annotations
-
-
-from typing import Any
+from typing import Any, Optional
 import os
 import pandas as pd
 from pymongo.mongo_client import MongoClient
@@ -14,58 +6,55 @@ import json
 from ensure import ensure_annotations
 
 
-class mongo_operation:
-    __collection=None # here i have created a private/protected variable
-    __database=None
+class MongoOperation:
+    __collection = None  # private/protected variable
+    __database = None
     
-    def __init__(self,client_url: str, database_name: str, collection_name: str=None):
-        self.client_url=client_url
-        self.database_name=database_name
-        self.collection_name=collection_name
+    def __init__(self, client_url: str, database_name: str, collection_name: Optional[str] = None):
+        self.client_url = client_url
+        self.database_name = database_name
+        self.collection_name = collection_name
        
-    def create_mongo_client(self,collection=None):
-        client=MongoClient(self.client_url)
-        return client
+    def create_mongo_client(self) -> MongoClient:
+        return MongoClient(self.client_url)
     
-    def create_database(self,collection=None):
-        if mongo_operation.__database==None:
-            client=self.create_mongo_client(collection)
-            self.database=client[self.database_name]
-        return self.database 
+    def create_database(self) -> Any:
+        if MongoOperation.__database is None:
+            client = self.create_mongo_client()
+            MongoOperation.__database = client[self.database_name]
+        return MongoOperation.__database
     
-    def create_collection(self,collection=None):
-        if mongo_operation.__collection==None:
-            database=self.create_database(collection)
-            self.collection=database[self.collection_name]
-            mongo_operation.__collection=collection
+    def create_collection(self, collection_name: Optional[str] = None) -> Any:
+        if collection_name is None:
+            collection_name = self.collection_name
         
-        if mongo_operation.__collection!=collection:
-            database=self.create_database(collection)
-            self.collection=database[self.collection_name]
-            mongo_operation.__collection=collection
-            
-        return self.collection
+        if MongoOperation.__collection is None or MongoOperation.__collection != collection_name:
+            database = self.create_database()
+            MongoOperation.__collection = database[collection_name]
+        
+        return MongoOperation.__collection
     
-    def insert_record(self,record: dict, collection_name: str) -> Any:
-        if type(record) == list:
+    def insert_record(self, record: Any, collection_name: str) -> None:
+        if isinstance(record, list):
             for data in record:
-                if type(data) != dict:
-                    raise TypeError("record must be in the dict")    
-            collection=self.create_collection(collection_name)
+                if not isinstance(data, dict):
+                    raise TypeError("Each record in the list must be a dictionary")
+            collection = self.create_collection(collection_name)
             collection.insert_many(record)
-        elif type(record)==dict:
-            collection=self.create_collection(collection_name)
+        elif isinstance(record, dict):
+            collection = self.create_collection(collection_name)
             collection.insert_one(record)
+        else:
+            raise TypeError("Record must be a dictionary or a list of dictionaries")
     
-    def bulk_insert(self,datafile,collection_name:str=None):
-        self.path=datafile
+    def bulk_insert(self, datafile: str, collection_name: Optional[str] = None) -> None:
+        if datafile.endswith('.csv'):
+            dataframe = pd.read_csv(datafile, encoding='utf-8')
+        elif datafile.endswith(".xlsx"):
+            dataframe = pd.read_excel(datafile, encoding='utf-8')
+        else:
+            raise ValueError("Unsupported file type. Use .csv or .xlsx files.")
         
-        if self.path.endswith('.csv'):
-            pd.read.csv(self.path,encoding='utf-8')
-            
-        elif self.path.endswith(".xlsx"):
-            dataframe=pd.read_excel(self.path,encoding='utf-8')
-            
-        datajson=json.loads(dataframe.to_json(orient='record'))
-        collection=self.create_collection()
-        collection.insert_many(datajson)
+        data_json = json.loads(dataframe.to_json(orient='records'))
+        collection = self.create_collection(collection_name)
+        collection.insert_many(data_json)
--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPHEN_E_DOT = '-e .'
 
-def get_requiremet(file_path:str)->List[str]:
+def get_requirements(file_path: str) -> List[str]:
     requirements = []
     with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements = f.readlines()
+        requirements = [req.strip() for req in requirements]
         
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
     return requirements
 
-print(get_requiremet("./requirements.txt"))
+print(get_requirements("./requirements.txt"))
